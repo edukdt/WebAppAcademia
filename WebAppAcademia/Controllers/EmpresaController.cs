963b358d21ee2549b4ab41e5d8758240f3b380ef
Eduardo Cruz <edukdt@live.com>
2021-11-18 15:58:45 -0300

    Adicionando o cadastro de empresa

---
+++ WebAppAcademia/Controllers/EmpresaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppAcademia.Models;
+
+namespace WebAppAcademia.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        // GET: EmpresaController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
